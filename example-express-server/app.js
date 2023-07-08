3f1c2591d706934fc74db4ca388e5b6098b3ff96
gcount85 <100mgml@gmail.com>
2023-07-08 23:36:56 +0900

    feat : Implement logger middleware
    -

--- example-express-server/app.js
+++ example-express-server/app.js
@@ -1,9 +1,18 @@
 /* basic express server config */
 
 const express = require('express');
+const config = require('./config');
 
 const app = express();
-const port = 9000;
+
+// 로깅을 위한 미들웨어
+const LoggerMiddleware = (req, res, next) => {
+  console.log(`Logges ${req.url} ${req.method}
+	-- ${new Date()}`);
+  next();
+};
+
+app.use(LoggerMiddleware); // 미들웨어를 로드하기 위해 필요
 
 app.get('/', (req, res) => {
   res.send('hello world');
@@ -20,9 +29,9 @@ app.all('/secret', (req, res, next) => {
 });
 
 // bind and listen to the connection on the host and port
-app.listen(PORT, (err) => {
+app.listen(config.PORT, (err) => {
   if (err) {
     console.log('Error in server setup');
   }
-  console.log('Server listening on port', PORT);
+  console.log('Server listening on port', config.PORT);
 });
---
+++ example-express-server/config.js
@@ -0,0 +1,3 @@
+const PORT = 8080;
+
+module.exports.PORT = PORT;
