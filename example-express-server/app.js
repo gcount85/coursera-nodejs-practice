40ba4d703c8c004dc6d7b471be29fb6ac0721f6c
gcount85 <100mgml@gmail.com>
2023-07-08 23:23:42 +0900

    feat : Add basic routes
    -

--- example-express-server/app.js
+++ example-express-server/app.js
@@ -5,6 +5,20 @@ const express = require('express');
 const app = express();
 const port = 9000;
 
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.post('/', (req, res) => {
+  res.send('POST request will get called');
+});
+
+// 미들웨어
+app.all('/secret', (req, res, next) => {
+  console.log('Accessing the secret section...');
+  next(); // next handler를 호출함
+});
+
 // bind and listen to the connection on the host and port
 app.listen(PORT, (err) => {
   if (err) {
