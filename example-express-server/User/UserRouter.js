6512c187500d6cd8085e25d260c2c4daeeee3af0
gcount85 <100mgml@gmail.com>
2023-07-09 11:44:28 +0900

    feat : Implement get route in UserRouter
    -

--- example-express-server/User/UserRouter.js
+++ example-express-server/User/UserRouter.js
@@ -1,1 +1,22 @@
-// all the routes
+// all the routes. 모든 요청을 적절한 controller 함수에게 보낸다.
+
+const express = require('express');
+const routes = express.Router();
+
+const userController = require('./UserController');
+
+/* 유저 데이터를 가져오는 라우트 */
+routes.get('/', (req, res) => {
+  try {
+    userController.getUsers((err, result) => {
+      if (err) {
+        return res.status(400).send(err);
+      }
+      return res.status(200).send({ status: 'OK', data: result });
+    });
+  } catch (err) {
+    return res.status(500).send('Try after sometime');
+  }
+});
+
+module.exports = routes;
