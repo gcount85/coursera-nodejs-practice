9a5d8315683a68484093dd2c44bd329b5f714db0
gcount85 <100mgml@gmail.com>
2023-07-09 11:28:44 +0900

    feat : Add UsersService.js
    -

--- example-express-server/User/UsersService.js
+++ example-express-server/User/UsersService.js
@@ -1,1 +1,9 @@
-// 모든 비즈니스 로직 코드
+// 모든 비즈니스 로직 코드 & DAO layer 호출
+
+const userDAO = require('./UsersDAO');
+
+const getUsers = (done) => {
+  userDAO.getUsers(done);
+};
+
+module.exports = { getUsers };
