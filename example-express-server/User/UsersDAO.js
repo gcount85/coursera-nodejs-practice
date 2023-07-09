c79965942bc9ac4a4b0cf19a008885b51c1b7e92
gcount85 <100mgml@gmail.com>
2023-07-09 11:25:44 +0900

    feat : Add UsersDAO.js
    -

--- example-express-server/User/UsersDAO.js
+++ example-express-server/User/UsersDAO.js
@@ -1,1 +1,16 @@
-// 데이터에 대한 모든 연산
+// 데이터에 대한 모든 연산 (Data Access Object)
+
+const fs = require('fs');
+
+const getUsers = (done) => {
+  fs.readFile('./users.json', (err, fileContent) => {
+    if (err) {
+      return done('Encountered error while getting users details');
+    }
+
+    let userData = JSON.parse(fileContent);
+    return done(undefined, userData); // error, result
+  });
+};
+
+module.exports = { getUsers };
